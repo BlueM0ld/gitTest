9726d9ba1f8d514dfa379103aaf81ae9d26943bd
LDeiriPS <lilia.deiri@publicissapient.com>
2022-10-06 11:42:32 +0100

    update line 10

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App=()=>{
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to ... 
+          Edit <code>src/App.js</code> and save to enhancement 
         </p>
         <a
           className="App-link"
